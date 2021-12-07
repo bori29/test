86f15a75ba2b0de9f1187485a31e7f4b871bb5e5
boris.postolenko <bori29@yandex.ru>
2021-12-07 22:42:23 +0300

    init commit

---
+++ src/ru/test/Calc.java
@@ -0,0 +1,8 @@
+package ru.test;
+
+public class Calc {
+    public static void main(String[] args) {
+        Test test = new Test();
+        System.out.println(test.calculator(6, 6));
+    }
+}
---
+++ src/ru/test/Test.java
@@ -0,0 +1,20 @@
+package ru.test;
+
+public class Test {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 3;
+        int summa = x + y;
+        String plus = " + ";
+        String ravno= " = ";
+        System.out.println(x + plus + y + ravno + summa);
+        Test test = new Test();
+        System.out.println(test.calculator(x, y));
+    }
+
+    public String calculator(int x, int y){
+        int summa = x + y;
+        return "Результат сложения " + x + " и " + y + " = " + summa;
+    }
+
+}
